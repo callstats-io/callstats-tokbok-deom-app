1b85b07a9221f3901c31aa1a0c025bf73585338e
Pallab <pallab.gain@gmail.com>
2019-10-17 15:21:54 +0300

    Add docker support Can run with self signed certificate

--- app/helloworld.py
+++ app/helloworld.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 from opentok import OpenTok
 import os
 
+__API_KEY = os.getenv('TOKBOX_API_KEY', '')
+__API_SECRET = os.getenv('TOKBOX_AUTH_TOKEN', '')
+__APP_ID = os.getenv('CALLSTATS_APP_ID', '')
+__APP_SECRET = os.getenv('CALLSTATS_APP_SECRET', '')
+__PORT = os.getenv("PORT", 7070)
+__TARGET = os.getenv('TARGET', 'dev')
+
 try:
-    api_key = os.environ['API_KEY']
-    api_secret = os.environ['API_SECRET']
+    api_key = __API_KEY
+    api_secret = __API_SECRET
 except Exception:
     raise Exception('You must define API_KEY and API_SECRET environment variables')
 
@@ -12,13 +19,17 @@ app = Flask(__name__)
 opentok = OpenTok(api_key, api_secret)
 session = opentok.create_session()
 
+
 @app.route("/")
 def hello():
     key = api_key
     session_id = session.session_id
     token = opentok.generate_token(session_id)
-    return render_template('index.html', api_key=key, session_id=session_id, token=token)
+    return render_template('index.html', api_key=key, session_id=session_id, token=token,
+                           csio_app_id=__APP_ID, csio_app_secret=__APP_SECRET)
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    isDebug = False
+    if __TARGET == 'local':
+        isDebug = True
+    app.run(host='0.0.0.0', port=__PORT, debug=isDebug, ssl_context='adhoc')
--- app/static/js/helloworld.js
+++ app/static/js/helloworld.js
@@ -5,17 +5,18 @@ var session = OT.initSession(apiKey, sessionId);
 var publisher = OT.initPublisher('publisher');
 
 const InitCallback = (t) => {
-  console.warn('callstats initialized ', t);
+    console.warn('callstats initialized ', t);
 }
 const StatsCallback = (t) => {
-  console.warn('callstats stats callback ', t);
+    console.warn('callstats stats callback ', t);
+    window.csioStats = t;
 }
 const payload = {
-  AppId: 'callstats_app_id',
-  AppSecret: 'callstats_app_secret',
-  SessionId: sessionId,
-  InitCallback: InitCallback,
-  StatsCallback: StatsCallback,
+    AppId: csioAppId,
+    AppSecret: csioAppSecret,
+    SessionId: sessionId,
+    InitCallback: InitCallback,
+    StatsCallback: StatsCallback,
 }
 var connection = CallstatsOpenTok.initialize(payload);
 
@@ -23,39 +24,39 @@ console.warn('callstats is ', connection, payload);
 // Attach event handlers
 session.on({
 
-  // This function runs when session.connect() asynchronously completes
-  sessionConnected: function(event) {
-    // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
-    // clients)
-    session.publish(publisher, err => {
-      if (error) {
-        console.error('Failed to publish', error);
-      }
-    });
-  },
+    // This function runs when session.connect() asynchronously completes
+    sessionConnected: function (event) {
+        // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
+        // clients)
+        session.publish(publisher, err => {
+            if (error) {
+                console.error('Failed to publish', error);
+            }
+        });
+    },
 
-  // This function runs when another client publishes a stream (eg. session.publish())
-  streamCreated: function(event) {
-    // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
-    // the element with id="subscribers"
-    var subContainer = document.createElement('div');
-    subContainer.id = 'stream-' + event.stream.streamId;
-    document.getElementById('subscribers').appendChild(subContainer);
+    // This function runs when another client publishes a stream (eg. session.publish())
+    streamCreated: function (event) {
+        // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
+        // the element with id="subscribers"
+        var subContainer = document.createElement('div');
+        subContainer.id = 'stream-' + event.stream.streamId;
+        document.getElementById('subscribers').appendChild(subContainer);
 
-    // Subscribe to the stream that caused this event, put it inside the container we just made
-    session.subscribe(event.stream, subContainer, (error) => {
-      if (error) {
-        console.error('Failed to subscribe', error);
-      }
-    });
+        // Subscribe to the stream that caused this event, put it inside the container we just made
+        session.subscribe(event.stream, subContainer, (error) => {
+            if (error) {
+                console.error('Failed to subscribe', error);
+            }
+        });
 
-  }
+    }
 
 });
 
 // Connect to the Session using a 'token' for permission
 session.connect(token, (error) => {
-  if (error) {
-    console.error('Failed to connect', error);
-  }
+    if (error) {
+        console.error('Failed to connect', error);
+    }
 });
